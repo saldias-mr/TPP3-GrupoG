c7118c71d6b744ed622d8feeb7a9424e601f1d21
Lucio Elvira <lucioelvira@hotmail.com>
2026-05-07 14:40:49 -0300

    HDR agencia cambios visuales
    Ajuste de diseño en RecepcionHDRAgencia y nuevo evento
    
    Se reorganizaron posiciones, tamaños y márgenes de controles para un diseño más compacto en el formulario RecepcionHDRAgencia. Se redujo el tamaño de la ventana y se agregó el evento Click para el botón "Confirmar recepción".

--- TPP3 GrupoG/RecepcionHDRAgencia/RecepcionHDRAgencia.Designer.cs
+++ TPP3 GrupoG/RecepcionHDRAgencia/RecepcionHDRAgencia.Designer.cs
@@ -36,9 +36,9 @@
             textBox3 = new TextBox();
             tipoHDR = new Label();
             listView1 = new ListView();
-            guiasAsociadas = new Label();
             numGuia = new ColumnHeader();
             direccion = new ColumnHeader();
+            guiasAsociadas = new Label();
             button1 = new Button();
             button2 = new Button();
             SuspendLayout();
@@ -46,79 +46,78 @@
             // numHDR
             // 
             numHDR.AutoSize = true;
-            numHDR.Location = new Point(32, 19);
+            numHDR.Location = new Point(24, 11);
+            numHDR.Margin = new Padding(2, 0, 2, 0);
             numHDR.Name = "numHDR";
-            numHDR.Size = new Size(79, 25);
+            numHDR.Size = new Size(51, 15);
             numHDR.TabIndex = 0;
             numHDR.Text = "Nº HDR:";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(117, 19);
+            textBox1.Location = new Point(82, 11);
+            textBox1.Margin = new Padding(2, 2, 2, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(156, 31);
+            textBox1.Size = new Size(110, 23);
             textBox1.TabIndex = 1;
             // 
             // buscarNumHDR
             // 
-            buscarNumHDR.Location = new Point(313, 19);
+            buscarNumHDR.Location = new Point(219, 11);
+            buscarNumHDR.Margin = new Padding(2, 2, 2, 2);
             buscarNumHDR.Name = "buscarNumHDR";
-            buscarNumHDR.Size = new Size(112, 34);
+            buscarNumHDR.Size = new Size(78, 30);
             buscarNumHDR.TabIndex = 2;
             buscarNumHDR.Text = "Buscar";
             buscarNumHDR.UseVisualStyleBackColor = true;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(117, 79);
+            textBox2.Location = new Point(82, 47);
+            textBox2.Margin = new Padding(2, 2, 2, 2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(156, 31);
+            textBox2.Size = new Size(110, 23);
             textBox2.TabIndex = 3;
             // 
             // fleteroTXT
             // 
             fleteroTXT.AutoSize = true;
-            fleteroTXT.Location = new Point(41, 82);
+            fleteroTXT.Location = new Point(24, 50);
+            fleteroTXT.Margin = new Padding(2, 0, 2, 0);
             fleteroTXT.Name = "fleteroTXT";
-            fleteroTXT.Size = new Size(70, 25);
+            fleteroTXT.Size = new Size(46, 15);
             fleteroTXT.TabIndex = 4;
             fleteroTXT.Text = "Fletero:";
             fleteroTXT.Click += fleteroTXT_Click;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(117, 144);
+            textBox3.Location = new Point(82, 86);
+            textBox3.Margin = new Padding(2, 2, 2, 2);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(156, 31);
+            textBox3.Size = new Size(110, 23);
             textBox3.TabIndex = 5;
             // 
             // tipoHDR
             // 
             tipoHDR.AutoSize = true;
-            tipoHDR.Location = new Point(51, 150);
+            tipoHDR.Location = new Point(24, 89);
+            tipoHDR.Margin = new Padding(2, 0, 2, 0);
             tipoHDR.Name = "tipoHDR";
-            tipoHDR.Size = new Size(51, 25);
+            tipoHDR.Size = new Size(34, 15);
             tipoHDR.TabIndex = 6;
             tipoHDR.Text = "Tipo:";
             // 
             // listView1
             // 
             listView1.Columns.AddRange(new ColumnHeader[] { numGuia, direccion });
-            listView1.Location = new Point(117, 253);
+            listView1.Location = new Point(22, 155);
+            listView1.Margin = new Padding(2, 2, 2, 2);
             listView1.Name = "listView1";
-            listView1.Size = new Size(203, 146);
+            listView1.Size = new Size(284, 89);
             listView1.TabIndex = 7;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.Details;
-            // 
-            // guiasAsociadas
-            // 
-            guiasAsociadas.AutoSize = true;
-            guiasAsociadas.Location = new Point(117, 225);
-            guiasAsociadas.Name = "guiasAsociadas";
-            guiasAsociadas.Size = new Size(55, 25);
-            guiasAsociadas.TabIndex = 8;
-            guiasAsociadas.Text = "Guías";
             // 
             // numGuia
             // 
@@ -130,29 +129,42 @@
             direccion.Text = "Dirección";
             direccion.Width = 90;
             // 
+            // guiasAsociadas
+            // 
+            guiasAsociadas.AutoSize = true;
+            guiasAsociadas.Location = new Point(22, 138);
+            guiasAsociadas.Margin = new Padding(2, 0, 2, 0);
+            guiasAsociadas.Name = "guiasAsociadas";
+            guiasAsociadas.Size = new Size(36, 15);
+            guiasAsociadas.TabIndex = 8;
+            guiasAsociadas.Text = "Guías";
+            // 
             // button1
             // 
-            button1.Location = new Point(117, 447);
+            button1.Location = new Point(22, 277);
+            button1.Margin = new Padding(2, 2, 2, 2);
             button1.Name = "button1";
-            button1.Size = new Size(174, 62);
+            button1.Size = new Size(76, 25);
             button1.TabIndex = 9;
             button1.Text = "Confirmar recepción";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(313, 475);
+            button2.Location = new Point(102, 277);
+            button2.Margin = new Padding(2, 2, 2, 2);
             button2.Name = "button2";
-            button2.Size = new Size(112, 34);
+            button2.Size = new Size(76, 25);
             button2.TabIndex = 10;
             button2.Text = "Salir";
             button2.UseVisualStyleBackColor = true;
             // 
             // RecepcionHDRAgencia
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(444, 521);
+            ClientSize = new Size(443, 313);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(guiasAsociadas);
@@ -164,6 +176,7 @@
             Controls.Add(buscarNumHDR);
             Controls.Add(textBox1);
             Controls.Add(numHDR);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "RecepcionHDRAgencia";
             Text = "Recepción de Hojas de Ruta (Agencia)";
             ResumeLayout(false);
--- TPP3 GrupoG/RecepcionHDRAgencia/RecepcionHDRAgencia.cs
+++ TPP3 GrupoG/RecepcionHDRAgencia/RecepcionHDRAgencia.cs
@@ -19,5 +19,10 @@ namespace TPP3_GrupoG.RecepcionHDRAgencia
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
